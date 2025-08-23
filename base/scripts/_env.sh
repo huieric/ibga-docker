c45fc97fcc43c70472fe5926793470389ba1277c
huieric <huieric2015@gmail.com>
2025-08-23 23:32:36 +0800

    simplify novnc

--- base/scripts/_env.sh
+++ base/scripts/_env.sh
@@ -6,13 +6,5 @@ IBG_PORT="${IBG_PORT:-8888}"
 IBG_EXEC=$(find $IBG_DIR -type f -name "ibgateway" -executable)
 IBG_DOWNLOAD_URL="${IBG_DOWNLOAD_URL:-https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/ibgateway-latest-standalone-linux-x64.sh}"
 IBGA_LOG_EXPORT_DIR="${IBGA_LOG_EXPORT_DIR:-$IBG_SETTINGS_DIR/exported_logs}"
-XVFB_PIDFILE=/var/run/xvfb.pid
-VNC_PIDFILE=/var/run/x11vnc.pid
-VNC_LOGFILE=/var/log/x11vnc.log
-NOVNC_DIR=$(find /opt -maxdepth 1 -type d -name "noVNC*")
-NOVNC_PIDFILE=/var/run/novnc.pid
 SOCAT_PIDFILE=/var/run/socat.pid
-DISPLAY=:0
 JAUTO_INPUT=/tmp/ibg-jauto.in
-
-export DISPLAY
--- base/scripts/manager.sh
+++ base/scripts/manager.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+export DISPLAY=:0
+
+# Clear previous lockfile
+rm -f /tmp/.X0-lock
+
+# Start VNC server
+setsid Xvnc -SecurityTypes None -AlwaysShared=1 -geometry 1920x1080 :0 &
+
+# Start noVNC server
+setsid ./noVNC/utils/novnc_proxy --vnc localhost:5900 &
+
+# Start openbox
+setsid openbox &
+
 if [ ! -z "$IB_TIMEZONE" ]; then
     sudo ln -fs /usr/share/zoneinfo/${IB_TIMEZONE// /_} /etc/localtime
     sudo dpkg-reconfigure -f noninteractive tzdata >/dev/null 2>&1
@@ -7,7 +21,6 @@ fi
 
 source $(dirname "$BASH_SOURCE")/_env.sh
 source $(dirname "$BASH_SOURCE")/_utils.sh
-source $(dirname "$BASH_SOURCE")/_run_xv.sh
 source $(dirname "$BASH_SOURCE")/_run_socat.sh
 source $(dirname "$BASH_SOURCE")/_run_ibg.sh
 
@@ -22,8 +35,5 @@ MSG="------------------------------------------------
 "
 _info "$MSG"
 
-_run_xvfb
-_run_vnc
-_run_novnc
 _run_socat
 _run_ibg
