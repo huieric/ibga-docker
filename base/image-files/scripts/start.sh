4e9c5f7f0b33d29339a3652b4d502814be01a51a
huieric <huieric2015@gmail.com>
2025-08-27 17:17:46 +0800

    fix monitor log path

--- base/image-files/scripts/start.sh
+++ base/image-files/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 day=`date +%Y%m%d`
-nohup /opt/ibga/monitor_ibg.sh > monitor_$day.log 2>&1 &
+nohup /opt/ibga/monitor_ibg.sh > $IBGA_LOG_EXPORT_DIR/monitor_$day.log 2>&1 &
 exec /opt/ibga/manager.sh 2>&1 | tee -a $IBGA_LOG_EXPORT_DIR/docker_$day.log
